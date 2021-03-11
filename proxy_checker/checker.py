3cdad7a13392a1ae689890b39049ba30879d7c5a
dorahero <66933004+dorahero@users.noreply.github.com>
2021-03-11 20:49:24 +0800

    first commit

--- proxy_checker/checker.py
+++ proxy_checker/checker.py
@@ -2,6 +2,8 @@ from model.proxyModel import ProxyModel
 import urllib.request
 import socket
 import urllib.error
+import requests
+from proxy_checker import ProxyChecker as PCh
 
 class ProxyChecker:
     def __init__(self, proxies: list) -> None:        
@@ -31,24 +33,51 @@ class ProxyChecker:
             
     @staticmethod
     def is_bad_proxy(uri, proxy):
+        url = "http://forum.gamer.com.tw/ajax/rank.php?c=21&page=1"
+        ip = proxy.split(':')[0]
+        port = proxy.split(':')[-1]
+        proxies = {"{}".format(uri): f"http://{ip}:{port}"}
+        
+        headers = {"User-Agent": "Mozilla/5.0"}
         try:
-            proxy_handler = urllib.request.ProxyHandler({uri: proxy})
-            opener = urllib.request.build_opener(proxy_handler)
-            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-            urllib.request.install_opener(opener)
-            req=urllib.request.Request('https://www.google.com')
-            req1=urllib.request.Request('https://www.facebook.com')
-            req2=urllib.request.Request('https://script.google.com/macros/s/AKfycby9S1Vx3ipI0DhQvZU5hJXOLyFpKErT9NPZwIUEI0RgssJVyV8/exec')
-            sock=urllib.request.urlopen(req)
-            sock=urllib.request.urlopen(req1)
-            sock=urllib.request.urlopen(req2)
-        except urllib.error.HTTPError as e:
-            print('Error code: ', e.code)
-            return e.code
-        except Exception as detail:
-            print("ERROR:", detail)
+            res = requests.get(url, proxies=proxies, headers=headers, timeout=2)
+            if res.status_code == 200:
+                print(proxy + "  works!")
+                return False
+            else:
+                print(proxy + " not working")
+                return True
+        except Exception as e:
+            print(proxy + " Not Working :( => " + str(e))
             return True
-        return False
+        # checker = PCh()
+
+        # ip = proxy.split(':')[0]
+        # port = proxy.split(':')[-1]
+        # c = checker.check_proxy('{}:{}'.format(ip, port))
+        # if c and c["timeout"] < 500:
+        #     print(c)
+        #     return False
+        # else:
+        #     return True
+        # try:
+        #     proxy_handler = urllib.request.ProxyHandler({uri: proxy})
+        #     opener = urllib.request.build_opener(proxy_handler)
+        #     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        #     urllib.request.install_opener(opener)
+        #     req=urllib.request.Request('https://www.google.com')
+        #     req1=urllib.request.Request('https://www.facebook.com')
+        #     req2=urllib.request.Request('https://script.google.com/macros/s/AKfycby9S1Vx3ipI0DhQvZU5hJXOLyFpKErT9NPZwIUEI0RgssJVyV8/exec')
+        #     sock=urllib.request.urlopen(req)
+        #     sock=urllib.request.urlopen(req1)
+        #     sock=urllib.request.urlopen(req2)
+        # except urllib.error.HTTPError as e:
+        #     print('Error code: ', e.code)
+        #     return e.code
+        # except Exception as detail:
+        #     print("ERROR:", detail)
+        #     return True
+        # return False
 
 
 if __name__ == "__main__":
--- start_checker.sh
+++ start_checker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --network proxy_network dorahero2727/proxy_checker:v1
+docker run --rm --network mongo_proxy_network dorahero2727/proxy_checker:v1
